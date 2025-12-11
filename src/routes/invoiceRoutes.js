7a49aeab31c28d5843694eac2e03f94fd626af43
leanclientportal <leanclientportal@gmail.com>
2025-12-11 11:44:28 +0000

    add invoice edit router

--- src/routes/invoiceRoutes.js
+++ src/routes/invoiceRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Joi = require('joi');
 const { protect } = require('../middlewares/auth');
 const {
   getInvoices,
@@ -8,19 +9,42 @@ const {
   deleteInvoice,
   markAsPaid
 } = require('../controllers/invoiceController');
+const {
+  validate,
+  validateParams,
+  validationSchemas,
+  objectIdSchema
+} = require('../middlewares/validation');
 
 // All routes are protected
 // router.use(protect);
 
+const projectParamSchema = Joi.object({
+  projectId: objectIdSchema.required()
+});
+
+const invoiceParamSchema = Joi.object({
+  projectId: objectIdSchema.required(),
+  invoiceId: objectIdSchema.required()
+});
+
 router.route('/:projectId')
-  .get(getInvoices)
-  .post(createInvoice);
+  .get(validateParams(projectParamSchema), getInvoices)
+  .post(
+    validateParams(projectParamSchema),
+    validate(validationSchemas.invoice.create),
+    createInvoice
+  );
 
 router.route('/:projectId/:invoiceId')
-  .put(updateInvoice)
-  .delete(deleteInvoice);
+  .put(
+    validateParams(invoiceParamSchema),
+    validate(validationSchemas.invoice.update),
+    updateInvoice
+  )
+  .delete(validateParams(invoiceParamSchema), deleteInvoice);
 
 router.route('/:projectId/:invoiceId/pay')
-  .put(markAsPaid);
+  .put(validateParams(invoiceParamSchema), markAsPaid);
 
 module.exports = router;
